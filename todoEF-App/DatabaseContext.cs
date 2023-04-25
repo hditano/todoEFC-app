b10cf62d509d64ff883fe004bd0873791559ab1c
Hernan Di Tano <hditano@gmail.com>
2023-04-25 13:36:31 +0200

    Added SearchUser method / CreateNewUser method/ a somewhat pretty Main Menu

--- todoEF-App/DatabaseContext.cs
+++ todoEF-App/DatabaseContext.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using todoEF_App.Models;
 
 namespace todoEF_App
 {
--- todoEF-App/Program.cs
+++ todoEF-App/Program.cs
@@ -1,7 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.EntityFrameworkCore;
 using todoEF_App;
+using todoEF_App.Models;
 
+static void MainMenu()
+{
+    bool isActive = true;
+
+    while (isActive)
+    {
+        Console.Clear();
+        Console.WriteLine("Welcome to todoEF-app");
+        Console.WriteLine("Please choose your option");
+        Console.WriteLine("-------------------------");
+        Console.WriteLine("1. Create new User");
+        Console.WriteLine("2. List all Users");
+        Console.WriteLine("3. Search for Username");
+        Console.WriteLine("4. Quit Program");
+        Console.WriteLine("");
+        Console.Write("Type your option: ");
+        string option = Console.ReadLine();
+
+        switch (option)
+        {
+            case "1":
+                CreateNewUser();
+                break;
+            case "2":
+                ListUsers();
+                break;
+            case "3":
+                Console.Write("Type userName: ");
+                string user = Console.ReadLine();
+                SearchUser(user);
+                break;
+            case "4":
+                isActive = false;
+                break;
+            case "default":
+                break;
+        }
+    }
+
+}
 
 static void ListUsers()
 {
@@ -11,14 +52,54 @@ static void ListUsers()
 
         List<User> users = con.Users.ToList();
 
-        foreach(User user in users)
+        Console.Clear();
+        foreach (User user in users)
         {
             Console.WriteLine($"Name: {user.FirstName} {user.LastName}");
             Console.WriteLine($"username: {user.userName}");
         }
+        Console.ReadKey();
     }
 
 }
 
+static void CreateNewUser()
+{
+    using (var con = new DatabaseContext())
+    {
+        Console.Write("Please write your First Name: ");
+        string fName = Console.ReadLine();
+        Console.Write("Please write your Last Name: ");
+        string lName = Console.ReadLine();
+        Console.Write("Please write your desired username: ");
+        string uName = Console.ReadLine();
 
-ListUsers();
+        var newUser = new User { FirstName = fName, LastName = lName, userName = uName };
+
+        con.Add(newUser);
+        con.SaveChanges();
+
+        Console.WriteLine("New User Created");
+
+    }
+}
+
+static void SearchUser(string username)
+{
+    using (var con = new DatabaseContext())
+    {
+        List<User> users = con.Users.Where(b => b.userName == username).ToList();
+
+        Console.Clear();
+        Console.WriteLine($"User Information for {username}");
+        Console.WriteLine("");
+        foreach (var user in users)
+        {
+            Console.WriteLine($"{user.FirstName} {user.LastName}");
+            Console.WriteLine($"{user.UserId}");
+        }
+        Console.ReadKey();
+    }
+}
+
+MainMenu();
